4780d46690d3da927e20fa1436f5a54791bf9dea
Orilly <brunocollingridge@gmail.com>
2021-02-26 10:57:07 +0000

    Reorganised Code

--- src/main/java/com/brunocollingridge/foodeffects/core/event/EventsHub.java
+++ src/main/java/com/brunocollingridge/foodeffects/core/event/EventsHub.java
@@ -20,48 +20,48 @@ public class EventsHub {
 		   ItemStack stack = event.getItem();
 		   PlayerEntity player = (PlayerEntity) event.getEntity();
 		   
-		// Cookie, Speed II for 5 Seconds
-	        if (stack.isItemEqual(stack) && stack.getItem()==Items.COOKIE && stack.getUseAction() == UseAction.EAT) {
-	        	player.addPotionEffect(new EffectInstance(Effects.SPEED,(100), 1));
-	        }
+		   // Eating Dried Kelp now clears the poison, nausea, and blindness effects.
+		   if (stack.isItemEqual(stack) && stack.getItem() == Items.DRIED_KELP && stack.getUseAction() == UseAction.EAT) {
+			   player.removePotionEffect(Effects.POISON);
+			   player.removePotionEffect(Effects.NAUSEA);
+			   player.removePotionEffect(Effects.BLINDNESS);
+		   }
 		   
-	    // Honey Bottle, Instant Health I
-	        if (stack.isItemEqual(stack) && stack.getItem()==Items.HONEY_BOTTLE && stack.getUseAction() == UseAction.DRINK) {
-	        	player.addPotionEffect(new EffectInstance(Effects.INSTANT_HEALTH, 0));
-	        }
-	        
-	    // Mushroom Stew, Regeneration II for 5 Seconds
-	        if (stack.isItemEqual(stack) && stack.getItem()==Items.MUSHROOM_STEW && stack.getUseAction() == UseAction.EAT) {
-	        	player.addPotionEffect(new EffectInstance(Effects.REGENERATION,(100), 1));
-	        }
-	        
-	    // Beetroot Soup, Health Boost for 30 Seconds
-	        if (stack.isItemEqual(stack) && stack.getItem()==Items.BEETROOT_SOUP && stack.getUseAction() == UseAction.EAT) {
-	        	player.addPotionEffect(new EffectInstance(Effects.HEALTH_BOOST,(600), 1/2));
-	        }
+		   // Eating a Pufferfish gives the effect Water Breathing for 120 seconds.
+		   if (stack.isItemEqual(stack) && stack.getItem() == Items.PUFFERFISH && stack.getUseAction() == UseAction.EAT) {
+			   player.addPotionEffect(new EffectInstance(Effects.WATER_BREATHING,2400, 0));
+		   }
 		   
-	    // Rabbit Stew, Jump Boost II for 10 Seconds
-	        if (stack.isItemEqual(stack) && stack.getItem()==Items.RABBIT_STEW && stack.getUseAction() == UseAction.EAT) {
-	        	player.addPotionEffect(new EffectInstance(Effects.JUMP_BOOST,(200), 1));
-	        }
+		   // Eating a Mushroom Stew gives the effect Regeneration II for 5 seconds.
+		   if (stack.isItemEqual(stack) && stack.getItem() == Items.MUSHROOM_STEW && stack.getUseAction() == UseAction.EAT) {
+			   player.addPotionEffect(new EffectInstance(Effects.REGENERATION,100, 1));
+		   }
 		   
-	    // Pumpkin Pie, Resistance for 30 seconds
-	        if (stack.isItemEqual(stack) && stack.getItem()==Items.PUMPKIN_PIE && stack.getUseAction() == UseAction.EAT) {
-	        	player.addPotionEffect(new EffectInstance(Effects.RESISTANCE,(400), 0));
-	        }
-	        
-	    // Pufferfish, Water Breathing for 1 Minute
-	        if (stack.isItemEqual(stack) && stack.getItem()==Items.PUFFERFISH && stack.getUseAction() == UseAction.EAT) {
-	        	player.addPotionEffect(new EffectInstance(Effects.WATER_BREATHING,(2400), 0));
-	        }
-	        
-	    // Dried Kelp, Clears Poison & Nausea & Blindness
-	        if (stack.isItemEqual(stack) && stack.getItem()==Items.DRIED_KELP && stack.getUseAction() == UseAction.EAT) {
-	        	player.removePotionEffect(Effects.POISON);
-	        	player.removePotionEffect(Effects.NAUSEA);
-	        	player.removePotionEffect(Effects.BLINDNESS);
-	        } 
-	        
+		   // Eating a Rabbit Stew gives the effect Jump Boost II for 10 seconds.
+		   if (stack.isItemEqual(stack) && stack.getItem() == Items.RABBIT_STEW && stack.getUseAction() == UseAction.EAT) {
+			   player.addPotionEffect(new EffectInstance(Effects.JUMP_BOOST,200, 1));
+		   }
+		   
+		   // Eating a Beetroot Soup gives the effect Health Boost 1/2 for 30 seconds.
+		   if (stack.isItemEqual(stack) && stack.getItem() == Items.BEETROOT_SOUP && stack.getUseAction() == UseAction.EAT) {
+			   player.addPotionEffect(new EffectInstance(Effects.HEALTH_BOOST,600, 0));
+		   }
+		   
+		   
+		   // Eating a Cookie gives the effect Speed I for 5 seconds.
+		   if (stack.isItemEqual(stack) && stack.getItem() == Items.COOKIE && stack.getUseAction() == UseAction.EAT) {
+			   player.addPotionEffect(new EffectInstance(Effects.SPEED,100, 0));
+		   }
+		   
+		   // Eating a Pumpkin Pie gives the effect Haste I for 15 seconds.
+		   if (stack.isItemEqual(stack) && stack.getItem() == Items.PUMPKIN_PIE && stack.getUseAction() == UseAction.EAT) {
+			   player.addPotionEffect(new EffectInstance(Effects.HASTE,300, 1));
+		   }
+		   
+		   // Drinking a Bottle of Honey gives the effect Instant Health.
+		   if (stack.isItemEqual(stack) && stack.getItem() == Items.HONEY_BOTTLE && stack.getUseAction() == UseAction.DRINK) {
+			   player.addPotionEffect(new EffectInstance(Effects.INSTANT_HEALTH));
+		   }
 
 	    }
        
